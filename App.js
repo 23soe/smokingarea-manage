3a4ecc20c78082c0ffed207f088e3a07c1f60454
김수경 <skim519@naver.com>
2023-03-28 19:43:00 +0900

    수정

--- App.js
+++ App.js
@@ -3,17 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Join from "./Join";
-import Login from "./Login";
+import join from "./join";
+import login from "./login";
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Join" component={Join}/>
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen name="login" component={login}/>
+        <Stack.Screen name="join" component={join}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
