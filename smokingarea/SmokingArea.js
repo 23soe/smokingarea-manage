9e720f025258c169994732bbe0aa8797f949ed89
23soe <102648428+23soe@users.noreply.github.com>
2023-04-03 19:49:36 +0900

    add non-smokingarea alert

--- smokingarea/SmokingArea.js
+++ smokingarea/SmokingArea.js
@@ -6,7 +6,8 @@ import {
   Text, 
   Button,
   ScrollView,
-  Dimensions} from 'react-native';
+  Dimensions,
+  TouchableOpacity} from 'react-native';
 
 const {width : SCREEN_WIDTH} = Dimensions.get("window");
 
@@ -20,43 +21,81 @@ const App = () => {
         showsHorizontalScrollIndicator={false}
         >
         <View style={styles.container}>
-        <Text style={styles.paragraph}>4F</Text>
-        <Image
-            style={styles.image}
-            source={require('../assets/S06-4F_smokingarea.jpg')}
-             resizeMode="contain"/>
-        <Button
-            title={showImage ? 'Hide Image' : 'Show Image'}
-            onPress={() => setShowImage(!showImage)}
-        /> 
-        {showImage && (
-        <Image
-          source={require('../assets/S06-4F_photo.jpg')}
-          style={styles.image}
-          resizeMode="contain"
-          
-        />
-      )}
-      </View>
+          <Text style={styles.paragraph}>B1F</Text>
+          <Image
+              style={styles.image}
+              source={require('../assets/S06-4F.jpg')}
+              resizeMode="contain"/>
+          <TouchableOpacity
+              onPress={() => alert('지하1층 흡연신고가 접수되었습니다.') }>
+              <Image
+                style={{
+                  left:125,
+                  bottom:200,
+                  width: 50,
+                  height: 50,
+                  borderRadius: 100,
+                  overflow: 'hidden',
+                }}
+                source={require('../assets/No_Smoking.png')}
+              />
+      </TouchableOpacity>
+        </View>
+
+
         <View style={styles.container}>
-        <Text style={styles.paragraph}>6F</Text>
-        <Image
+          <Text style={styles.paragraph}>4F</Text>
+          <Image
+              style={styles.image}
+              source={require('../assets/S06-4F_smokingarea.jpg')}
+              resizeMode="contain"/>
+          <Button
+              title={showImage ? 'Hide Image' : 'Show Image'}
+              onPress={() => setShowImage(!showImage)}
+          /> 
+          {showImage && (
+          <Image
+            source={require('../assets/S06-4F_photo.jpg')}
             style={styles.image}
-            source={require('../assets/s06-6F_smokingarea.jpg')}
+            resizeMode="contain" 
+          />
+        )}
+      </View>
+
+
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>6F</Text>
+          <Image
+              style={styles.image}
+              source={require('../assets/S06-6F_smokingarea.jpg')}
             resizeMode="contain"/>
+            <TouchableOpacity
+              onPress={() => alert('6층 흡연신고가 접수되었습니다.') }>
+              <Image
+                style={{
+                  left:155,
+                  bottom:272,
+                  width: 30,
+                  height: 30,
+                  borderRadius: 100,
+                  overflow: 'hidden',
+                }}
+                source={require('../assets/No_Smoking.png')}
+              />
+      </TouchableOpacity>
         <Button
             title={showImage ? 'Hide Image' : 'Show Image'}
             onPress={() => setShowImage(!showImage)}
         /> 
         {showImage && (
         <Image
-          source={require('../assets/s06-6f_photo.jpg')}
+          source={require('../assets/S06-6F_photo.jpg')}
           style={styles.image}
           resizeMode="contain"
         />
       )}
       </View>
-      </ScrollView>
+    </ScrollView>
   );
 };
 
